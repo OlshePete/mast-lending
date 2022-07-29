2399f84bc27c72c0b304287915f907b5aa91a913
OlshePete <olshevskii.pv@gmail.com>
2022-07-29 17:44:54 +0300

    Updates

---
+++ precache-manifest.4f51d01f89733e3b69d9a269cef7c34e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "06886ed06e9238ab45e6",
+    "url": "/mast-lending/static/css/main.3392bb35.chunk.css"
+  },
+  {
+    "revision": "06886ed06e9238ab45e6",
+    "url": "/mast-lending/static/js/main.06886ed0.chunk.js"
+  },
+  {
+    "revision": "ec45cc027b1dbcc42480",
+    "url": "/mast-lending/static/js/1.ec45cc02.chunk.js"
+  },
+  {
+    "revision": "05464f5de0c8228f91de",
+    "url": "/mast-lending/static/js/runtime~main.05464f5d.js"
+  },
+  {
+    "revision": "6cf94d9979e3438c6ef392f890a92183",
+    "url": "/mast-lending/static/media/mast-header-logo-dark.6cf94d99.png"
+  },
+  {
+    "revision": "fa02592d6c6d1b81bb99cff2a8fec28d",
+    "url": "/mast-lending/static/media/mast-logo-text-dark.fa02592d.png"
+  },
+  {
+    "revision": "a76b97bb7b117f50d896e9d7c4b93315",
+    "url": "/mast-lending/static/media/test_image.a76b97bb.png"
+  },
+  {
+    "revision": "adfa7a5e6d7ee036911ea300a86c07a1",
+    "url": "/mast-lending/static/media/mast-general-logo.adfa7a5e.png"
+  },
+  {
+    "revision": "6775f7b3e48eb32fd9f7d3f4ad08c80e",
+    "url": "/mast-lending/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mast-lending/precache-manifest.4f51d01f89733e3b69d9a269cef7c34e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mast-lending/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
